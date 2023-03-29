919f03e85ec1b010082ce44e9934a1e24a896de8
MarioG314 <garciamd91@gmail.com>
2023-03-29 09:54:17 -0300

    inicializo app con expo

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  Image,
+  Alert,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Esto se encuentra dentro de una etiqueta "Text"</Text>
+      <View style={styles.containerBtn}>
+        <Button
+          style={styles.btnAdd}
+          color="#008000"
+          onPress={() => Alert.alert("Agregaste + 1")}
+          title="Click Me"
+        />
+        <Button
+          style={styles.btnSub}
+          color="#FF0000"
+          onPress={() => Alert.alert("Mensaje de alerta!")}
+          title="Click Me"
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +35,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  btnAdd: { borderBottomColor: "#000000" },
+  btnSub: { borderBottomColor: "#000000" },
+  containerBtn: {
+    flex: 0.04,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: "#800080",
+    height: "50px",
   },
 });
